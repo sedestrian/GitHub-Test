17d07fa853d018eda53161559849313d728b4551
Alessandro <bipolar@hotmail.it>
2019-07-10 00:23:14 +0200

    Missed files from previous commit

--- app/src/main/java/com/gaboardi/githubtest/api/usersquery/UsersQuery.kt
+++ app/src/main/java/com/gaboardi/githubtest/api/usersquery/UsersQuery.kt
@@ -1,7 +1,12 @@
-package com.gaboardi.githubtest.data.api.usersquery
+package com.gaboardi.githubtest.api.usersquery
 
+import com.gaboardi.githubtest.model.UserQueryBody
+import com.gaboardi.githubtest.model.UserQueryResponse
+import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
 
-class UsersQuery {
-  @GET("")
+interface UsersQuery {
+  @GET("/search/users")
+  fun call(@Body body: UserQueryBody): Call<UserQueryResponse>
 }
