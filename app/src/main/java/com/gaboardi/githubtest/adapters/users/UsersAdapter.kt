386b75f4c586528141c9c2d9ddcd9e48c4787ea7
Alessandro <bipolar@hotmail.it>
2019-07-12 00:32:46 +0200

    Refactoring adapter for pagination

--- app/src/main/java/com/gaboardi/githubtest/adapters/users/UsersAdapter.kt
+++ app/src/main/java/com/gaboardi/githubtest/adapters/users/UsersAdapter.kt
@@ -10,18 +10,46 @@ import androidx.recyclerview.widget.RecyclerView
 import com.gaboardi.githubtest.R
 import com.gaboardi.githubtest.databinding.UserItemBinding
 import com.gaboardi.githubtest.model.User
+import com.gaboardi.githubtest.model.base.NetworkState
 import com.gaboardi.githubtest.util.AppExecutors
 import com.gaboardi.githubtest.view.common.DataBoundListAdapter
 
 class UsersAdapter(
     val appExecutors: AppExecutors,
-    val onCLick: ((user: User) -> Unit)?
+    val onCLick: ((user: User) -> Unit)?,
+    val onRetry: () -> Unit
 ): PagedListAdapter<User, RecyclerView.ViewHolder>(
-    AsyncDifferConfig.Builder<T>(diffCallback)
+    AsyncDifferConfig.Builder(User.diffItemCallback)
         .setBackgroundThreadExecutor(appExecutors.diskIO())
         .build()
 ){
-    override fun createBinding(parent: ViewGroup): UserItemBinding {
+    private var networkState: NetworkState? = null
+    private fun hasExtraRow() = networkState != null && networkState != NetworkState.LOADED
+
+    override fun getItemViewType(position: Int): Int {
+        return if (hasExtraRow() && position == itemCount - 1) {
+            R.layout.network_state_item
+        } else {
+            R.layout.user_item
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (getItemViewType(position)) {
+            R.layout.reddit_post_item -> (holder as RedditPostViewHolder).bind(getItem(position))
+            R.layout.network_state_item -> (holder as NetworkStateItemViewHolder).bindTo(
+                networkState)
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int, payloads: MutableList<Any>) {
+
+    }
+    /*override fun createBinding(parent: ViewGroup): UserItemBinding {
         val binding = DataBindingUtil.inflate<UserItemBinding>(
             LayoutInflater.from(parent.context),
             R.layout.user_item,
@@ -36,6 +64,5 @@ class UsersAdapter(
         binding.root.setOnClickListener {
             onCLick?.invoke(item)
         }
-    }
-
+    }*/
 }
